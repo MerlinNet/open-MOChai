255343b5b45928e1af974a5c6a684c00af94b130
铭茗 <3029590078@qq.com>
2026-04-17 18:28:08 +0800

    优化夜间效果和同步脚本

--- sync_to_documents.sh
+++ sync_to_documents.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# 同步项目到 Documents 目录
-rsync -av --delete \
+# 同步项目到 Documents 目录（只同步有变化的文件）
+# rsync -c 使用校验和比较，确保只传输真正变化的文件
+rsync -avc --delete \
   --exclude '.git' \
   --exclude '.godot' \
   ~/WorkSpace/open-MOChai/ ~/storage/shared/Documents/open-MOChai/
